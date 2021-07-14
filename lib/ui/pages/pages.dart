c4900f1b37ccedef06ba555339685b11ec2d1285
Aziz Alfauzi <50761836+Azizalfauzi@users.noreply.github.com>
2021-07-14 14:59:07 +0700

    adding signin,signuppage and much more

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:food_market_app/shared/shared.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 part 'general_page.dart';
 part 'signin_page.dart';
+part 'signup_page.dart';
--- lib/ui/pages/signup_page.dart
+++ lib/ui/pages/signup_page.dart
@@ -1,25 +1,71 @@
 part of 'pages.dart';
 
-class SignInPage extends StatefulWidget {
-  const SignInPage({Key? key}) : super(key: key);
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
-  _SignInPageState createState() => _SignInPageState();
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class _SignInPageState extends State<SignInPage> {
-  bool isLoading = false;
+class _SignUpPageState extends State<SignUpPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return GeneralPage(
-      title: "Sign In",
-      subtitle: "Find your best ever meal",
+      title: "Sign Up",
+      subtitle: "Register and eat",
+      onBackButtonPressed: () {
+        Get.back();
+      },
       child: Column(
         children: [
           Container(
-            margin: EdgeInsets.fromLTRB(defaultMargin, 52, defaultMargin, 6),
+            width: 110,
+            height: 110,
+            margin: EdgeInsets.only(top: 26),
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/photo_border.png'))),
+            child: Container(
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://firebasestorage.googleapis.com/v0/b/login-app-6b3d6.appspot.com/o/pics%2F9E9TKm33PrSjCBIKf2oGc54SDV73?alt=media&token=14eabbf8-8367-4b96-9aee-cd6b9d031d5a'),
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(defaultMargin, 26, defaultMargin, 6),
+            width: double.infinity,
+            child: Text(
+              'Name',
+              style: blackFontStyle2,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.black),
+            ),
+            child: TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintStyle: greyTextFont,
+                hintText: "Type your full name",
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(defaultMargin, 26, defaultMargin, 6),
             width: double.infinity,
             child: Text(
               'Email Address',
@@ -70,40 +116,13 @@ class _SignInPageState extends State<SignInPage> {
           ),
           Container(
             width: double.infinity,
-            margin: EdgeInsets.only(top: defaultMargin * 3),
-            height: 50,
-            padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-            child: isLoading
-                ? SpinKitFadingCircle()
-                : ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(mainColor),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          side: BorderSide.none,
-                        ),
-                      ),
-                    ),
-                    child: Text(
-                      'Sign In',
-                      style: GoogleFonts.poppins().copyWith(
-                          color: Colors.black, fontWeight: FontWeight.w500),
-                    ),
-                  ),
-          ),
-          Container(
-            width: double.infinity,
-            margin: EdgeInsets.only(top: defaultMargin),
+            margin: EdgeInsets.only(top: defaultMargin * 2),
             height: 50,
             padding: EdgeInsets.symmetric(horizontal: defaultMargin),
             child: ElevatedButton(
-              onPressed: () {
-                Get.to(SignUpPage());
-              },
+              onPressed: () {},
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(greyColor),
+                backgroundColor: MaterialStateProperty.all(mainColor),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8.0),
@@ -112,9 +131,9 @@ class _SignInPageState extends State<SignInPage> {
                 ),
               ),
               child: Text(
-                'Create New Account',
+                'Continue',
                 style: GoogleFonts.poppins()
-                    .copyWith(color: Colors.white, fontWeight: FontWeight.w500),
+                    .copyWith(color: Colors.black, fontWeight: FontWeight.w500),
               ),
             ),
           ),
